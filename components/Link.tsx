a088623b8c260765b54b59b3293bd03d8a026040
maekawa-syuki <syu54117123>
2021-06-17 17:52:10 +0900

    link add

---
+++ components/Link.tsx
@@ -0,0 +1,26 @@
+﻿import { motion } from 'framer-motion'
+import React, {VFC} from 'react'
+import styled from 'styled-components'
+import Link from "next/link"
+
+const LinkWrapper = styled(motion.div)`
+
+`
+const Links = styled.a`
+    cursor: pointer;
+`
+type Props ={
+    id:string
+}
+
+const LinkPage:VFC<Props> = ({id}) => {
+    return (
+        <>
+            <LinkWrapper>
+                <Link href="/posts/[id]" as={`/posts/${id}`}><Links>From {id}</Links></Link>
+            </LinkWrapper>
+        </>
+    )
+}
+
+export default LinkPage
---
+++ pages/posts/[id].tsx
@@ -0,0 +1,16 @@
+﻿import { useRouter } from "next/router"
+import Link from "next/link"
+
+
+const Content = () => {
+    const router = useRouter();
+    return (
+        <>
+            <Link href="/" >Back To Home</Link>
+            <h1></h1>
+            <p>挨拶</p>
+        </>
+    )
+}
+
+export default Content
