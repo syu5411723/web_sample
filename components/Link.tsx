944e2017f86fd514a693d220a8e31f4fb047b273
maekawa-syuki <syu54117123>
2021-06-17 18:31:18 +0900

    router edit

--- components/Link.tsx
+++ components/Link.tsx
@@ -1,5 +1,5 @@
 ﻿import { motion } from 'framer-motion'
-import React, {VFC} from 'react'
+import React, { VFC } from 'react'
 import styled from 'styled-components'
 import Link from "next/link"
 
@@ -9,14 +9,24 @@ const LinkWrapper = styled(motion.div)`
 const Links = styled.a`
     cursor: pointer;
 `
-type Props ={
-    id:string
+type Props = {
+    id: string
+}
+const wrapperV = {
+    hidden: { opacity: 0, x: "-50px" },
+    visible: { opacity: 1, x: "0px", transition: { duration: 1, delay: 0.5 } },
+    exit: { opacity: 0, x: "50px", transition: { duration: 1, delay: 0.5 } }
 }
 
-const LinkPage:VFC<Props> = ({id}) => {
+const LinkPage: VFC<Props> = ({ id }) => {
     return (
         <>
-            <LinkWrapper>
+            <LinkWrapper
+                variants={wrapperV}
+                initial="hidden"
+                animate="visible"
+                exit="exit"
+            >
                 <Link href="/posts/[id]" as={`/posts/${id}`}><Links>From {id}</Links></Link>
             </LinkWrapper>
         </>
--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,14 +1,29 @@
 ﻿import { useRouter } from "next/router"
 import Link from "next/link"
+import styled from "styled-components"
+import { motion } from "framer-motion"
 
+const Wrapper = styled(motion.div)``
 
+const wrapperV = {
+    hidden: {opacity: 0, x: "-50px"},
+    visible: {opacity: 1, x: "0px", transition: {duration:1, delay:0.5} },
+    exit: {opacity: 0, x: "50px", transition: {duration: 1, delay:0.5}}
+}
 const Content = () => {
     const router = useRouter();
     return (
         <>
+            <Wrapper
+                variants={wrapperV}
+                initial="hidden"
+                animate="visible"
+                exit="exit"
+            >
             <Link href="/" >Back To Home</Link>
             <h1></h1>
             <p>挨拶</p>
+            </Wrapper>
         </>
     )
 }
